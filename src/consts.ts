ec3792d2dc7973b4ce276ca92c95ef0eeafc910b
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2025-03-18 05:56:18 +0700

    update global

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'PMII Komfakdish';
+export const SITE_DESCRIPTION = 'Website Resmi PMII Komisariat Fakultas Dirasat Islamiyah Cabang Ciputat';
