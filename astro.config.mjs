15be5ce73ed029cfe4c096b6b8d05a8b7fb9b993
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2025-07-06 02:05:38 +0700

    fix image

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://pmiikomfakdish.or.id',
+	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 	vite: {
     plugins: [tailwindcss()],
