be43137e6181a97d5ad8149f5ebaa3fcdfd628de
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2025-07-06 09:37:36 +0700

    fix media folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://pmiikomfakdish.or.id',
 	integrations: [mdx(), sitemap()],
 	vite: {
     plugins: [tailwindcss()],
