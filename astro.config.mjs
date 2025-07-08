e1a4c7745f590704d5028eea07f8a9d7669941cd
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2025-07-08 22:52:11 +0700

    adding sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pmiikomfakdish.or.id',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), sanity(), react()],
 
   vite: {
   plugins: [tailwindcss()],
