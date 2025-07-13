48dc9f33bfad34da1f5664bb215225d38b9c0d71
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2025-07-14 03:58:23 +0700

    fix sanity error

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
 
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pmiikomfakdish.or.id',
-  integrations: [mdx(), sitemap(), sanity(), react()],
+  integrations: [mdx(), sitemap()],
 
   vite: {
   plugins: [tailwindcss()],
