9e6afde59759174ab81b4ef5cee89efd20a68c56
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2025-08-20 16:53:03 +0700

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-
-
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pmiikomfakdish.or.id',
+
   integrations: [mdx(), sitemap(), sanity({
       projectId: "ywo19okn",
       dataset: "production",
@@ -18,7 +18,10 @@ export default defineConfig({
       apiVersion: "2025-01-28",
       studioBasePath: "/admin",
     }), react()],
+
   vite: {
   plugins: [tailwindcss()],
 },
+
+  adapter: vercel(),
 });
